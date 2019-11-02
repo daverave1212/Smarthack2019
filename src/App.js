6dddb2101e0ba80e70eb6fe942d0077446221a7a
horatiuzetau <horatiu.atanasoaei16@gmail.com>
2019-11-02 13:10:14 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+
+
+
+
+
+  return (
+    <>
+    <h1 style={{textAlign:"center"}}>Editor</h1>
+    <div id="editor">
+      <iframe src="http://daverave1212.github.io/startbootstrap-small-business-master/index.html" style={{width: "100%", height: "100%"}}></iframe>
+    </div>
+    </>
+  );
+}
+
+export default App;
